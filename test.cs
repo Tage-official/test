5b42d9f0cb29789b81aed7f365b5f37783020b05
Maxim <maksim.grincevich@gmail.com>
2022-11-09 18:32:49 +0300

    first

--- test.cs
+++ test.cs
@@ -1,2 +1,18 @@
-let a = '2';
-console.log(a);
+using System;
+namespace Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("c=");
+            double c = double.Parse(Console.ReadLine());
+            double summ = a + b + c;
+            Console.WriteLine("{0}+{1}+{2}={3}", a, b, c, summ);
+        }
+    }
+}
